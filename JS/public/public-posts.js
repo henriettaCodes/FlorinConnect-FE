e9b9646dc919ceda2919784ba6ac34bc31ecc0b4
zephania <zephania1656@live.com>
2024-02-14 16:28:10 +0000

    styles

--- JS/public/public-posts.js
+++ JS/public/public-posts.js
@@ -26,11 +26,11 @@ document.addEventListener('DOMContentLoaded', async () => {
             const card = document.createElement('div')
             card.classList.add('card')
             card.innerHTML = `
-                <h2 style="text-align: center;">${exampleObject.title}</h2>
-                <p style="text-align: left; font-size: 15px; margin: 5px;">${exampleObject.category} posted by: ${exampleObject.author_username}</p>
-                <p style="text-align: right; font-size: 15px; margin-top: -20px;">${formatDateTime(exampleObject.date_posted)}</p>
+                <h2 class="top" style="text-align: center;">${exampleObject.title}</h2>
+                <p class="mid" style="text-align: left; font-size: 15px; margin: 5px;">${exampleObject.category} posted by: ${exampleObject.author_username}</p>
+                <p class="mid" style="text-align: right; font-size: 15px; margin-top: -20px;">${formatDateTime(exampleObject.date_posted)}</p>
                 <br>
-                <p style="text-align: center; font-size: 20px;">${exampleObject.content}</p>
+                <p class="bottom" style="text-align: center; font-size: 20px;">${exampleObject.content}</p>
             `
             cardContainer.appendChild(card)
         })
